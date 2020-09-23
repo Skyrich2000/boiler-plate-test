db4e9f7e47bc1613d3b503475eafdedfdd4415ac
Skyrich <petercha2000@gmail.com>
2020-09-23 21:02:15 +0900

    아잉

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://petercha2000:huncha8274007@boilerplate.4atua.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log("MongoDB Connected.."))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
